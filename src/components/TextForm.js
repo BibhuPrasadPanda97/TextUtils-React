8085c27ae64e2145e5057ccd92d5341c926590d0
BibhuPrasadPanda97 <bibhuprasad619.bpl@gmail.com>
2022-08-21 00:21:24 +0530

    Added 'Clear Text' and bug fixes regarding CLS(Cummulative Layout Shift)

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,6 +15,16 @@ export default function TextForm(props) {
         props.showAlert("Text changed to LowerCase", "success");
     }
 
+    // Handle copy text
+    const handleCopy = () => {
+        console.log("Text Copied!!");
+        var text = document.getElementById('textBoxInput');
+        text.select();
+        navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to Clipboard!", "success");
+    }
+
     const handleClearClick = () => {
         let newText = '';
         setText(newText);
@@ -34,10 +44,11 @@ export default function TextForm(props) {
             <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'darkgrey':'white', color: props.mode==='dark'?'white':'black'}} id="textBoxInput" rows="5"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#293d51':'white', color: props.mode==='dark'?'white':'black'}} id="textBoxInput" rows="5"></textarea>
                 </div>
                 <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Conver to UpperCase</button>
                 <button className="btn btn-primary mx-2 my-1" onClick={handleLpClick}>Conver to LowerCase</button>
+                <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
                 <button className="btn btn-primary mx-2 my-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className="container my-3" style={{color: props.mode==='dark'?'white':'black'}}>
